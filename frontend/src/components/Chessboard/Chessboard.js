8a9a00606d5d07d279570b66927bdb482a551aa5
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2022-08-14 23:42:48 +0300

    Created a fully completed chess board
    It now has correct spacing and colors

--- frontend/src/components/Chessboard/Chessboard.js
+++ frontend/src/components/Chessboard/Chessboard.js
@@ -1,10 +1,46 @@
 import styles from './Chessboard.module.css';
 
-const horizontalAxis = ['a','b','c','d','e','f','g'];
-const verticalAxis = [1,2,3,4,5,6,7,8];
+const verticalAxis = ['1','2','3','4','5','6','7','8'];
+const horizontalAxis = ['a','b','c','d','e','f','g','h'];
 
 export default function Chessboard(){
 
-    return <div id={styles.chessBoard}></div>
+    let board = [];
+
+    for(let j = verticalAxis.length - 1; j >= 0; j--){
+
+        for(let i = 0; i < horizontalAxis.length; i++){
+
+            const tileCounter = j + i;
+
+            if(tileCounter % 2 == 0){
+                board.push(
+                    <div class={styles.positionsBlack}>
+                        
+                    </div>
+                );
+            }
+            else{
+                board.push(
+                    <div class={styles.positionsWhite}>
+                        
+                    </div>
+                );
+            }
+            
+        }
+
+    }
+
+
+    return(
+    
+        <div id={styles.chessBoard}>
+
+            {board}
+
+        </div>
+    
+    )
 
 }
