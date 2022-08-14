6575810c29bdfe2179bc371cafddc6f0b6041a68
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2022-08-15 01:17:21 +0300

    Added peices to chess board
    Added complete peices to the chess board in proper starting positions

--- frontend/src/components/Chessboard/Chessboard.js
+++ frontend/src/components/Chessboard/Chessboard.js
@@ -4,6 +4,41 @@ import Tile from './Tile';
 const verticalAxis = ['1','2','3','4','5','6','7','8'];
 const horizontalAxis = ['a','b','c','d','e','f','g','h'];
 
+const peices = [];
+
+for(let i = 0; i < 8; i++){
+    
+    peices.push({image: "assets/images/pawn_b.png",x: i,y: 6,});
+
+}
+for(let i = 0; i < 8; i++){
+    
+    peices.push({image: "assets/images/pawn_w.png",x: i,y: 1,});
+
+}
+
+peices.push({image: "assets/images/rook_b.png",x: 0,y: 7,});
+peices.push({image: "assets/images/rook_b.png",x: 7,y: 7,});
+peices.push({image: "assets/images/rook_w.png",x: 0,y: 0,});
+peices.push({image: "assets/images/rook_w.png",x: 7,y: 0,});
+
+peices.push({image: "assets/images/knight_b.png",x: 1,y: 7,});
+peices.push({image: "assets/images/knight_b.png",x: 6,y: 7,});
+peices.push({image: "assets/images/knight_w.png",x: 1,y: 0,});
+peices.push({image: "assets/images/knight_w.png",x: 6,y: 0,});
+
+peices.push({image: "assets/images/bishop_b.png",x: 2,y: 7,});
+peices.push({image: "assets/images/bishop_b.png",x: 5,y: 7,});
+peices.push({image: "assets/images/bishop_w.png",x: 2,y: 0,});
+peices.push({image: "assets/images/bishop_w.png",x: 5,y: 0,});
+
+peices.push({image: "assets/images/king_b.png",x: 3,y: 7,});
+peices.push({image: "assets/images/king_w.png",x: 3,y: 0,});
+
+peices.push({image: "assets/images/queen_b.png",x: 4,y: 7,});
+peices.push({image: "assets/images/queen_w.png",x: 4,y: 0,});
+
+
 export default function Chessboard(){
 
     let board = [];
@@ -13,7 +48,18 @@ export default function Chessboard(){
         for(let i = 0; i < horizontalAxis.length; i++){
 
             const tileCounter = j + i;
-            board.push(<Tile tileCounter={tileCounter} />);
+
+            let image = undefined;
+
+            peices.forEach(p =>{
+
+                if(p.x === i && p.y === j){
+                    image = p.image;
+                }
+
+            })
+
+            board.push(<Tile tileCounter={tileCounter} images={image} />);
             
         }
 
--- frontend/src/components/Chessboard/Tile/Tile.js
+++ frontend/src/components/Chessboard/Tile/Tile.js
@@ -1,15 +1,23 @@
 import styles from './Tile.module.css';
 
-export default function tile({tileCounter}){
+export default function tile({tileCounter, images}){
 
     if(tileCounter % 2 === 0){
 
-        return <div className={styles.tileBlack}></div>
+        return (
+            <div className={styles.tileBlack}>
+                <img className={styles.chessImg} src={images} alt="" />
+            </div>
+        );
 
     }
     else{
 
-        return <div className={styles.tileWhite}></div>
+        return (
+            <div className={styles.tileWhite}>
+                <img className={styles.chessImg} src={images} alt="" />
+            </div>
+        );
 
     }
 
