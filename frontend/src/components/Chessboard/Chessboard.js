4c1e41334a539cb3713ddeb0068dc7ac9dcf8ca1
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2022-08-15 14:16:26 +0300

    Made a new folder for pieces and moved the piece render logic there

--- frontend/src/components/Chessboard/Chessboard.js
+++ frontend/src/components/Chessboard/Chessboard.js
@@ -1,41 +1,9 @@
 import styles from './Chessboard.module.css';
 import Tile from './Tile';
+import renderPeices from './Pieces/pieces.js';
 
 const verticalAxis = ['1','2','3','4','5','6','7','8'];
 const horizontalAxis = ['a','b','c','d','e','f','g','h'];
-
-const peices = [];
-
-for(let i = 0; i < 8; i++){
-    
-    peices.push({image: "assets/images/pawn_b.png",x: i,y: 6,});
-
-}
-for(let i = 0; i < 8; i++){
-    
-    peices.push({image: "assets/images/pawn_w.png",x: i,y: 1,});
-
-}
-
-for(let p = 0; p < 2; p++){
-
-    const type = (p === 0) ? "b" : "w";
-
-    const y = (p === 0) ? 7 : 0;
-
-    peices.push({image: `assets/images/rook_${type}.png`,x: 0,y: y,});
-    peices.push({image: `assets/images/rook_${type}.png`,x: 7,y: y,});
-
-    peices.push({image: `assets/images/knight_${type}.png`,x: 1,y: y,});
-    peices.push({image: `assets/images/knight_${type}.png`,x: 6,y: y,});
-
-    peices.push({image: `assets/images/bishop_${type}.png`,x: 2,y: y,});
-    peices.push({image: `assets/images/bishop_${type}.png`,x: 5,y: y,});
-
-    peices.push({image: `assets/images/queen_${type}.png`,x: 3,y: y,});
-    peices.push({image: `assets/images/king_${type}.png`,x: 4,y: y,});
-
-}
 
 export default function Chessboard(){
 
@@ -49,7 +17,7 @@ export default function Chessboard(){
 
             let image = undefined;
 
-            peices.forEach(p =>{
+            renderPeices().forEach(p =>{
 
                 if(p.x === i && p.y === j){
                     image = p.image;
---
+++ frontend/src/components/Chessboard/Pieces/pieces.js
@@ -0,0 +1,40 @@
+function renderPeices(){
+
+    const peices = [];
+
+    for(let i = 0; i < 8; i++){
+        
+        peices.push({image: "assets/images/pawn_b.png",x: i,y: 6,});
+
+    }
+    for(let i = 0; i < 8; i++){
+        
+        peices.push({image: "assets/images/pawn_w.png",x: i,y: 1,});
+
+    }
+
+    for(let p = 0; p < 2; p++){
+
+        const type = (p === 0) ? "b" : "w";
+
+        const y = (p === 0) ? 7 : 0;
+
+        peices.push({image: `assets/images/rook_${type}.png`,x: 0,y: y,});
+        peices.push({image: `assets/images/rook_${type}.png`,x: 7,y: y,});
+
+        peices.push({image: `assets/images/knight_${type}.png`,x: 1,y: y,});
+        peices.push({image: `assets/images/knight_${type}.png`,x: 6,y: y,});
+
+        peices.push({image: `assets/images/bishop_${type}.png`,x: 2,y: y,});
+        peices.push({image: `assets/images/bishop_${type}.png`,x: 5,y: y,});
+
+        peices.push({image: `assets/images/queen_${type}.png`,x: 3,y: y,});
+        peices.push({image: `assets/images/king_${type}.png`,x: 4,y: y,});
+
+    }
+
+    return peices;
+
+}
+
+export default renderPeices;
