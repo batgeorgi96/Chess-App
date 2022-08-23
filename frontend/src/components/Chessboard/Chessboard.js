cec11c145314c3647f95372166a71d81fece6c64
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2022-08-23 03:09:35 +0300

    implemented logic so that players cannot move chess pices outside the board

--- frontend/src/components/Chessboard/Chessboard.js
+++ frontend/src/components/Chessboard/Chessboard.js
@@ -1,59 +1,85 @@
 import styles from './Chessboard.module.css';
 import Tile from './Tile';
 import peices from './pieces/pieces.js';
+import {useRef} from 'react';
+
 
 const verticalAxis = ['1','2','3','4','5','6','7','8'];
 const horizontalAxis = ['a','b','c','d','e','f','g','h'];
 
-let grabedPiece = null;
+export default function Chessboard(){
+    
+    let board = [];
+    let grabedPiece = null;
+    const chessBoardRef = useRef(null);
 
-function grab(e){
-
-    const tileEl = e.target
-
-    if(tileEl.classList.contains("Tile_chessPiece__jEfn6")){
-
-        const x = e.clientX - 50;
-        const y = e.clientY - 50;
-
-        tileEl.style.position = "absolute";
-        tileEl.style.left = `${x}px`;
-        tileEl.style.top = `${y}px`;
-
-        grabedPiece = tileEl;
-
+    function grab(e){
+    
+        const tileEl = e.target
+    
+        if(tileEl.classList.contains("Tile_chessPiece__jEfn6")){
+    
+            const x = e.clientX - 50;
+            const y = e.clientY - 50;
+    
+            tileEl.style.position = "absolute";
+            tileEl.style.left = `${x}px`;
+            tileEl.style.top = `${y}px`;
+    
+            grabedPiece = tileEl;
+    
+        }
+        
     }
     
-}
+    function move(e){
+    
+        const chessboard = chessBoardRef.current;
 
-function move(e){
+        if(grabedPiece && chessboard){
+    
+            const minX = chessboard.offsetLeft - 30;
+            const minY = chessboard.offsetTop - 25;
 
-    if(grabedPiece){
+            const maxX = (chessboard.offsetLeft + chessboard.clientWidth) - 74;
+            const maxY = (chessboard.offsetTop + chessboard.clientHeight) - 78;
 
-        const x = e.clientX - 50;
-        const y = e.clientY - 50;
+            const x = e.clientX - 50;
+            const y = e.clientY - 50;
+    
+            grabedPiece.style.position = "absolute";
+            grabedPiece.style.left = `${x}px`;
+            grabedPiece.style.top = `${y}px`;
 
-        grabedPiece.style.position = "absolute";
-        grabedPiece.style.left = `${x}px`;
-        grabedPiece.style.top = `${y}px`;
+            console.log(chessboard.clientHeight)
 
+            if(x < minX){
+                grabedPiece.style.left = `${minX}px`
+            }
+            else if(x > maxX){
+                grabedPiece.style.left = `${maxX}px`
+            }
+
+            if(y < minY){   
+                grabedPiece.style.top = `${minY}px`
+            } 
+            else if(y > maxY){
+                if(y > maxY) grabedPiece.style.top = `${maxY}px`
+            }
+
+        }
+    
     }
-
-}
-
-function drop(e){
-
-    if(grabedPiece){
-
-        grabedPiece = null;
-
+    
+    function drop(e){
+    
+        if(grabedPiece){
+    
+            grabedPiece = null;
+    
+        }
+    
     }
-
-}
-
-export default function Chessboard(){
-
-    let board = [];
 
     for(let j = verticalAxis.length - 1; j >= 0; j--){
 
@@ -79,7 +105,12 @@ export default function Chessboard(){
 
     return(
     
-        <div onMouseDown={grab} onMouseMove={move} onMouseUp={drop}  id={styles.chessBoard}>
+        <div 
+        ref={chessBoardRef} 
+        onMouseDown={grab} 
+        onMouseMove={move} 
+        onMouseUp={drop}  
+        id={styles.chessBoard}>
 
             {board}
 
